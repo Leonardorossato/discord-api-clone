f2c54f6b65b5168003a0af442762c8e5b2ca3800
Leonardorossato <leonardorossato21@gmail.com>
2023-12-09 17:40:23 -0300

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,21 +3,24 @@ import { ConfigModule } from '@nestjs/config';
 import { MemberModule } from './member/member.module';
 import { ProfileModule } from './profile/profile.module';
 import { ServerModule } from './server/server.module';
-import { MemberModule } from './member/member.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    // GraphQLModule.forRootAsync({
-    //   inject: [],
-    //   imports: [],
-    //   driver: ApolloDriver,
-    //   useFactory: async () => ({
-    //     playground: true,
-    //     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-    //     sortSchema: true,
-    //     subscriptions: {},
-    //   }),
-    // }),
+    GraphQLModule.forRootAsync({
+      inject: [],
+      imports: [],
+      driver: ApolloDriver,
+      useFactory: async () => ({
+        playground: true,
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        sortSchema: true,
+        subscriptions: {},
+      }),
+    }),
     ServerModule,
     ProfileModule,
     MemberModule,
